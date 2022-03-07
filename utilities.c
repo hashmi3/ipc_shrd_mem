7dac17f931a61c604ca380ce598efa8467114374
hashmi3 <98249502+hashmi3@users.noreply.github.com>
2022-03-07 01:16:17 -0800

    chores: Cleaned up branch

---
+++ utilities.c
@@ -0,0 +1,25 @@
+#include "utilities.h"
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+unsigned long alignAddress(void *addr){
+  return (unsigned long) (addr + (sizeof(void*)-1UL)) & ~ 8UL;
+}
+
+char *getUser() {
+  /* Store information about the current user running the program*/
+  struct passwd *login;
+
+  /* Get the uid - from the Man Pages */
+  int id = getuid();
+  login = getpwuid(id);
+  if (!login) {
+    fprintf(stderr, "%sError parsing user information.%s\n", RED, COLOR_RESET);
+    exit(-1);
+  }
+  return login->pw_name;
+}
---
+++ utilities.h
@@ -0,0 +1,13 @@
+#ifndef __UTILS_H_
+#define __UTILS_H_
+
+#define BLUE "\x1b[34m"
+#define GREEN "\x1b[32m"
+#define TEAL "\x1b[36m"
+#define RED "\x1b[31m"
+#define COLOR_RESET "\x1b[0m"
+
+char *getUser();
+unsigned long alignAddress(void *addr);
+
+#endif // __UTILS_H_
